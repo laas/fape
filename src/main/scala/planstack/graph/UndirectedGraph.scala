d4a1a29729c984267b2442520e7373849e1d8012
Arthur Bit-Monnot <arthur.bit-monnot@laas.fr>
2014-01-16 17:32:13 +0100

    With a todo (and very small documentation)

--- src/main/scala/planstack/graph/UndirectedGraph.scala
+++ src/main/scala/planstack/graph/UndirectedGraph.scala
@@ -9,9 +9,23 @@ trait UndirectedGraph[V, E <: Edge[V]] extends Graph[V,E] {
    */
   def edges(v:V) : Seq[E]
 
+  /**
+   * Return all edges (u,v) and (v,u)
+   * @param u
+   * @param v
+   * @return
+   */
   def edges(u:V, v:V): Seq[E] = edges(u).filter(e => e.u == v || e.v == v)
+  //TODO: This is wrong if we are looking for edges (u,u)
 
   def degree(v:V) = edges(v).length
+
+  /**
+   * return all vertices u such as their is an edge (u,v) or (v,u)
+   * @param v
+   * @return
+   */
+  def neighbours(v:V) = edges(v).map(e => if(e.u != v) e.u else e.v).toSet
 }
 
 
