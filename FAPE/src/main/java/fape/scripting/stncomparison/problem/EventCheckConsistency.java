1a6a157b799850bcc66c82c8e80840408cc6434c
Filip Dvorak <filip@dvorak.fr>
2014-05-05 04:36:05 +0200

    adding environment for experimenting upon different constraint networks

---
+++ FAPE/src/main/java/fape/scripting/stncomparison/problem/EventCheckConsistency.java
@@ -0,0 +1,39 @@
+/*
+ * Author:  Filip Dvoøák <filip.dvorak@runbox.com>
+ *
+ * Copyright (c) 2013 Filip Dvoøák <filip.dvorak@runbox.com>, all rights reserved
+ *
+ * Publishing, providing further or using this program is prohibited
+ * without previous written permission of the author. Publishing or providing
+ * further the contents of this file is prohibited without previous written
+ * permission of the author.
+ */
+
+package fape.scripting.stncomparison.problem;
+
+import fape.core.planning.stn.STNManager;
+import fape.exceptions.FAPEException;
+import fape.scripting.stncomparison.fullstn.STNManagerOrig;
+import java.util.Random;
+
+/**
+ *
+ * @author FD
+ */
+public class EventCheckConsistency extends Event  {
+
+
+    @Override
+    public void Apply(STNManagerOrig o, Random rg) {
+        //o.IsConsistent();
+        //always consistent - determined during the insertion of a constraint
+    }
+
+    @Override
+    public void Apply(STNManager n, Random rg, STNScenario s) {
+        if(!n.IsConsistent()){
+            throw new FAPEException("inconsistent");
+        }        
+    }
+
+}
---
+++ FAPE/src/main/java/fape/scripting/stncomparison/problem/EventInsertVar.java
@@ -0,0 +1,36 @@
+/*
+ * Author:  Filip Dvoøák <filip.dvorak@runbox.com>
+ *
+ * Copyright (c) 2013 Filip Dvoøák <filip.dvorak@runbox.com>, all rights reserved
+ *
+ * Publishing, providing further or using this program is prohibited
+ * without previous written permission of the author. Publishing or providing
+ * further the contents of this file is prohibited without previous written
+ * permission of the author.
+ */
+
+package fape.scripting.stncomparison.problem;
+
+import fape.core.planning.stn.STNManager;
+import fape.scripting.stncomparison.fullstn.STNManagerOrig;
+import java.util.Random;
+import planstack.anml.model.concrete.TPRef;
+
+/**
+ *
+ * @author FD
+ */
+public class EventInsertVar extends Event  {
+
+    @Override
+    public void Apply(STNManagerOrig o, Random rg) {
+        o.getNewVariable();
+    }
+
+    @Override
+    public void Apply(STNManager n, Random rg, STNScenario s) {
+        n.recordTimePoint(new TPRef(s.varCount+2));
+        s.varCount++;
+    }
+
+}
