2e5b0c11ce0412139f079bd7e0aa614705d99f92
Arthur Bit-Monnot <arthur.bit-monnot@laas.fr>
2014-10-27 18:22:05 +0100

    Updated dependencies handling as they moved to openrobots.
    The cloned repositories in "planstack" must be updated with the new URL.

--- planstack/update.sh
+++ planstack/update.sh
@@ -3,26 +3,20 @@
 # clone dependencies in read-only mode
 # for read write access, you should replace git:// with ssh://
 if [ ! -d "graph" ]; then
-  git clone git://trac.laas.fr/git/robots/fape-graphs graph
+  git clone git://git.openrobots.org/robots/fape/fape-graphs.git graph
 fi
 
 if [ ! -d "constraints" ]; then
-  git clone git://trac.laas.fr/git/robots/fape-constraints constraints
+  git clone git://git.openrobots.org/robots/fape/fape-constraints.git constraints
 fi
 
 if [ ! -d "anml" ]; then
-  git clone git://trac.laas.fr/git/robots/fape-anml anml
+  git clone git://git.openrobots.org/robots/fape/fape-anml.git anml
 fi
 
 # for each repository, pull for latest changes, build and publish 
 # to local ivy store (should be accessible for building other SBT projects)
 
-cd graph
-git pull
-sbt publishLocal
-cd ../constraints
-git pull
-sbt publishLocal
-cd ../anml
-git pull
-sbt publishLocal
+cd graph && git pull && sbt publishLocal && \
+cd ../constraints && git pull && sbt publishLocal && \
+cd ../anml && git pull && sbt publishLocal
