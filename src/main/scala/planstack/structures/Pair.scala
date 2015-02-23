3d9647c1b4f3643e5416345efa6941eeea7a6125
Arthur Bit-Monnot <arthur.bit-monnot@laas.fr>
2015-02-23 17:50:54 +0100

    Short name for elements in Pair

--- src/main/scala/planstack/structures/Pair.scala
+++ src/main/scala/planstack/structures/Pair.scala
@@ -1,6 +1,6 @@
 package planstack.structures
 
-class Pair[T,V](val value1:T, val value2:V) {
+class Pair[T,V](val v1:T, val v2:V) {
 
-  override def toString = "(%s, %s)".format(value1, value2)
+  override def toString = "(%s, %s)".format(v1, v2)
 }
