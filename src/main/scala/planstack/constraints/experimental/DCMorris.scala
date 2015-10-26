9f6eb260bf741e7228c0cf82033516efe7c661b8
Arthur Bit-Monnot <arthur.bit-monnot@laas.fr>
2015-10-26 14:42:41 -0700

    Adding some examples

--- src/main/scala/planstack/constraints/experimental/DCMorris.scala
+++ src/main/scala/planstack/constraints/experimental/DCMorris.scala
@@ -69,6 +69,7 @@ class DCMorris {
     dcBackprop(source, propagated, callHistory, Right(negReq))
     for(e <- inEdges(source) if e.d < 0 && e.isInstanceOf[Upper])
       dcBackprop(source, propagated, callHistory, Left(e.asInstanceOf[Upper]))
+    propagated += source
     println("end: "+source)
   }
 
@@ -100,7 +101,7 @@ class DCMorris {
       if(!visited.contains(cur)) {
         visited += cur
 
-        if(dist > 0) {
+        if(dist >= 0) {
           addEdge(new Req(cur, source, dist))
         } else {
           if(isNegative(cur))
@@ -129,26 +130,107 @@ object DCMorrisTest extends App {
   val C = 3
   val D = 4
   val E: Node = 5
+  val F = 6
 
-  val stnu = new DCMorris()
-
-  val edges = List(
-    Req(E,B,4),
-    Upper(B, A, -2, B),
-    Lower(A, B, 0, B),
-    Req(B, D, 1),
-    Upper(D, C, -3, D),
-    Lower(C, D, 0, D),
-    Req(D, B, 3),
-    Upper(B, A, -2, B),
-    Lower(A, B, 0, B),
-    Req(B, E, -2)
+  val notDCs = List(
+    List(Req(A,B,5), Req(B,A,-6)),
+    List(Req(C, B, 3), Req(B, C, -1), Upper(B,A,-5,B), Lower(A,B,1,B)),
+    List(Req(C, B, 3), Req(B, C, -1), Upper(B,A,-5,B), Lower(A,B,1,B))
   )
-  for(e <- edges)
-    stnu.addEdge(e)
 
-  println()
+  val DCs = List(
+    List(Req(B,C, 5), Req(C,B, -5), Upper(B,A,-5,B), Lower(A,B,1,B))
+  )
 
-  val props = Buff[Node]()
-  stnu.dcBackprop(E, props, Nil)
+  def assertNotDC(edges: List[Edge]): Unit = {
+    val stnu = new DCMorris()
+    for(e <- edges)
+      stnu.addEdge(e)
+    assert(!stnu.determineDC())
+  }
+  def assertDC(edges: List[Edge]): Unit = {
+    val stnu = new DCMorris()
+    for(e <- edges)
+      stnu.addEdge(e)
+    assert(stnu.determineDC())
+  }
+//  for(notDC <- notDCs) {
+//    assertNotDC(notDC)
+//  }
+//  for(dc <- DCs) {
+//    assertDC(dc)
+//  }
+
+
+//  val stnu = new DCMorris()
+//
+//  val edges = List(
+//    Req(E,B,4),
+//    Upper(B, A, -2, B),
+//    Lower(A, B, 0, B),
+//    Req(B, D, 1),
+//    Upper(D, C, -3, D),
+//    Lower(C, D, 0, D),
+//    Req(D, B, 3),
+//    Upper(B, A, -2, B),
+//    Lower(A, B, 0, B),
+//    Req(B, E, -2)
+//  )
+//  for(e <- edges)
+//    stnu.addEdge(e)
+//
+//  println()
+//
+//  val props = Buff[Node]()
+//  try {
+//    stnu.dcBackprop(E, props, Nil)
+//    println("DC")
+//  } catch {
+//    case e:NotDC => println("Not DC")
+//  }
+
+
+  def makeNonObservable(edges: List[Edge], nodes: List[Node]) : List[Edge] = nodes match {
+    case Nil => edges
+    case h::tail => makeNonObservable(makeNonObservable(edges, h), tail)
+  }
+
+  def makeNonObservable(edges: List[Edge], node: Node) : List[Edge] = {
+    val upper = edges.find(e => e.isInstanceOf[Upper] && e.asInstanceOf[Upper].lbl == node).get
+    val lower = edges.find(e => e.isInstanceOf[Lower] && e.asInstanceOf[Lower].lbl == node).get
+
+    val l = lower.d
+    val u = upper.d
+    val src = lower.from
+
+    edges.filter(e => e != upper && e != lower).map {
+      case Req(`node`,y,d) => Req(src, y, l+d)
+      case Req(x,`node`,d) => Req(x, src, d+u)
+      case Lower(`node`, y, d, lbl) => Lower(src, y, l+d, lbl)
+      case Upper(x, `node`, d, lbl) => Upper(x, src, u+d, lbl)
+      case e => assert(e.from != node && e.to != node); e
+    }
+  }
+
+  val ex1 = List(
+    cont(A,B,0,5), reqs(B,C,20,25), reqs(B,D,20,25), cont(F,E,0,5), reqs(E,C,1,6), reqs(E,D,0,5)
+  ).flatten
+
+  println("\nObserve example:")
+  assertDC(ex1)
+
+  println("\nTwo non-obs:")
+  assertNotDC(makeNonObservable(ex1, List(B,E)))
+
+  println("\nOne non-obs:")
+  assertDC(makeNonObservable(ex1, List(E)))
+  assertNotDC(makeNonObservable(ex1, List(B)))
+
+
+  def cont(src:Node, dst: Node, minDur:Int, maxDur:Int) = {
+    List(Upper(dst, src, -maxDur, dst), Lower(src, dst, minDur, dst))
+  }
+  def reqs(src:Node, dst:Node, min:Int, max:Int) = {
+    List(Req(src, dst, max), Req(dst, src, -min))
+  }
 }
