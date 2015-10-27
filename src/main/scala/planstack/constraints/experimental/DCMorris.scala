10bbc7f6934de16554c5835864fa271b1dc48fbb
Arthur Bit-Monnot <arthur.bit-monnot@laas.fr>
2015-10-26 17:17:05 -0700

    Extraction of necessary observations

--- src/main/scala/planstack/constraints/experimental/DCMorris.scala
+++ src/main/scala/planstack/constraints/experimental/DCMorris.scala
@@ -1,5 +1,7 @@
 package planstack.constraints.experimental
 
+import planstack.constraints.experimental.DCMorris.Proj
+
 import scala.collection.mutable.{HashMap => MMap}
 import scala.collection.mutable.{ArrayBuffer => Buff}
 import scala.collection.mutable.{PriorityQueue => Queue}
@@ -7,14 +9,20 @@ import scala.collection.mutable.{Set => MSet}
 
 object DCMorris {
   type Node = Int
-  sealed abstract class Edge(val from: Node, val to: Node, val d: Int)
-  case class Req(override val from: Node, override val to: Node, override val d: Int) extends Edge(from, to, d)
-  case class Upper(override val from: Node, override val to: Node, override val d: Int, lbl: Node) extends Edge(from, to, d)
-  case class Lower(override val from: Node, override val to: Node, override val d: Int, lbl: Node) extends Edge(from, to, d)
+
+  sealed abstract class Proj(val n: Node)
+  case class MaxProj(override val n: Node) extends Proj(n)
+  case class MinProj(override val n: Node) extends Proj(n)
+
+  sealed abstract class Edge(val from: Node, val to: Node, val d: Int, val proj:Set[Proj])
+  case class Req(override val from: Node, override val to: Node, override val d: Int, override val proj:Set[Proj])
+    extends Edge(from, to, d, proj)
+  case class Upper(override val from: Node, override val to: Node, override val d: Int, lbl: Node, override val proj:Set[Proj]) extends Edge(from, to, d, proj)
+  case class Lower(override val from: Node, override val to: Node, override val d: Int, lbl: Node, override val proj:Set[Proj]) extends Edge(from, to, d, proj)
 
 }
 
-class NotDC extends Exception
+class NotDC(val involvedProjections: Set[Proj]) extends Exception
 
 class DCMorris {
   import DCMorris._
@@ -50,16 +58,20 @@ class DCMorris {
       .exists(e2 => e2.d <= e.d)
   }
 
+  private def extractNeedObs(projs: Set[Proj]) : Set[Node] = {
+    val maxProjs = projs.filter(p => p.isInstanceOf[MaxProj]).map(p => p.n)
+    projs.filter(p => p.isInstanceOf[MinProj] && maxProjs.contains(p.n)).map(p => p.n)
+  }
 
-  def determineDC(): Boolean = {
+  def determineDC(): (Boolean, Option[Set[Node]]) = {
     try {
       val propagated = Buff[Node]()
       for (n <- nodes; if isNegative(n))
         dcBackprop(n, propagated, Nil)
     } catch {
-      case e:NotDC => return false
+      case e:NotDC => return (false, Some(extractNeedObs(e.involvedProjections)))
     }
-    true
+    (true, None)
   }
 
   @throws[NotDC]
@@ -75,15 +87,15 @@ class DCMorris {
 
   @throws[NotDC]
   def dcBackprop(source: Node, propagated: Buff[Node], callHistory: List[Node], curEdges: Either[Upper, List[Req]]) : Unit = {
-    case class QueueElem(n: Node, dist: Int)
+    case class QueueElem(n: Node, dist: Int, projs: Set[Proj])
     def suitable(e: Edge) = (curEdges, e) match  {
-      case (Left(Upper(_, _, _, upLbl)) ,Lower(_, _, _, downLbl)) if upLbl == downLbl => false
+      case (Left(Upper(_, _, _, upLbl, _)) ,Lower(_, _, _, downLbl, _)) if upLbl == downLbl => false
       case _ => true
     }
 
     val visited = MSet[Node]()
-    if(callHistory contains source)
-      throw new NotDC
+    assert(!callHistory.contains(source), "This should have been caught earlier")
+
     if(propagated contains source)
       return ;
     val queue = Queue[QueueElem]()(Ordering.by(- _.dist))
@@ -93,21 +105,24 @@ class DCMorris {
       case Right(reqs) => reqs
     }
     for(e <- toProp)
-      queue += QueueElem(e.from, e.d)
+      queue += QueueElem(e.from, e.d, e.proj)
 
     while(queue.nonEmpty) {
-      val QueueElem(cur, dist) = queue.dequeue()
-      println(s"  dequeue: $cur ($dist)")
+      val QueueElem(cur, dist, projs) = queue.dequeue()
+      println(s"  dequeue: $cur ($dist) $projs")
       if(!visited.contains(cur)) {
         visited += cur
 
         if(dist >= 0) {
-          addEdge(new Req(cur, source, dist))
+          addEdge(new Req(cur, source, dist, projs))
         } else {
-          if(isNegative(cur))
+          if(isNegative(cur)) {
+            if(source == cur || callHistory.contains(cur))
+              throw new NotDC(projs)
             dcBackprop(cur, propagated, source :: callHistory)
+          }
           for(e <- inEdges(cur) ; if e.d >= 0 && suitable(e))
-            queue += QueueElem(e.from, dist + e.d)
+            queue += QueueElem(e.from, dist + e.d, projs ++ e.proj)
         }
       }
     }
@@ -133,26 +148,29 @@ object DCMorrisTest extends App {
   val F = 6
 
   val notDCs = List(
-    List(Req(A,B,5), Req(B,A,-6)),
-    List(Req(C, B, 3), Req(B, C, -1), Upper(B,A,-5,B), Lower(A,B,1,B)),
-    List(Req(C, B, 3), Req(B, C, -1), Upper(B,A,-5,B), Lower(A,B,1,B))
+    List(Req(A,B,5, Set()), Req(B,A,-6, Set())),
+    List(Req(C, B, 3, Set()), Req(B, C, -1, Set()), Upper(B,A,-5,B, Set()), Lower(A,B,1,B, Set())),
+    List(Req(C, B, 3, Set()), Req(B, C, -1, Set()), Upper(B,A,-5,B, Set()), Lower(A,B,1,B, Set()))
   )
 
   val DCs = List(
-    List(Req(B,C, 5), Req(C,B, -5), Upper(B,A,-5,B), Lower(A,B,1,B))
+    List(Req(B,C, 5, Set()), Req(C,B, -5, Set()), Upper(B,A,-5,B, Set()), Lower(A,B,1,B, Set()))
   )
 
   def assertNotDC(edges: List[Edge]): Unit = {
     val stnu = new DCMorris()
     for(e <- edges)
       stnu.addEdge(e)
-    assert(!stnu.determineDC())
+    val (dc,obs) = stnu.determineDC()
+    assert(!dc)
+    println(s"You should observe at least one of events: $obs")
   }
   def assertDC(edges: List[Edge]): Unit = {
     val stnu = new DCMorris()
     for(e <- edges)
       stnu.addEdge(e)
-    assert(stnu.determineDC())
+    val (dc,_) = stnu.determineDC()
+    assert(dc)
   }
 //  for(notDC <- notDCs) {
 //    assertNotDC(notDC)
@@ -200,37 +218,80 @@ object DCMorrisTest extends App {
     val lower = edges.find(e => e.isInstanceOf[Lower] && e.asInstanceOf[Lower].lbl == node).get
 
     val l = lower.d
+    val lowerProjs = lower.proj + MinProj(node)
     val u = upper.d
+    val upperProjs = upper.proj + MaxProj(node)
     val src = lower.from
 
     edges.filter(e => e != upper && e != lower).map {
-      case Req(`node`,y,d) => Req(src, y, l+d)
-      case Req(x,`node`,d) => Req(x, src, d+u)
-      case Lower(`node`, y, d, lbl) => Lower(src, y, l+d, lbl)
-      case Upper(x, `node`, d, lbl) => Upper(x, src, u+d, lbl)
+      case Req(`node`,y,d, projs) => Req(src, y, l+d, projs ++ lowerProjs)
+      case Req(x,`node`,d, projs) => Req(x, src, d+u, projs ++ upperProjs)
+      case Lower(`node`, y, d, lbl, projs) => Lower(src, y, l+d, lbl, projs ++ lowerProjs)
+      case Upper(x, `node`, d, lbl, projs) => Upper(x, src, u+d, lbl, projs ++ upperProjs)
       case e => assert(e.from != node && e.to != node); e
     }
+  }
+
+  def getMinimalObservationSets(edges: List[Edge]) : Iterable[Set[Node]] = {
+    val ctgs = edges.filter(_.isInstanceOf[Upper]).map(_.asInstanceOf[Upper].lbl).toSet
+    val solutions = MSet[Set[Node]]()
+    var queue = MSet[Set[Node]]()
+    queue += Set()
+
+    while(queue.nonEmpty) {
+      val candidate = queue.head
+      queue -= candidate
+
+      val nonObs = ctgs -- candidate
+      val allObsEdges = makeNonObservable(edges, nonObs.toList)
+
+      val stnu = new DCMorris()
+      for(e <- allObsEdges)
+        stnu.addEdge(e)
+
+      println(s"Candidate: $candidate")
+      stnu.determineDC() match {
+        case (true, None) =>
+          println("  Valid")
+          solutions.add(candidate)
+        case (false, Some(possiblyObservable)) =>
+          for(n <- possiblyObservable) {
+            println(s"new: ${candidate+n}")
+            queue += candidate + n
+          }
+      }
+    }
+    val minSols = solutions.filterNot(s => solutions.exists(s2 => s2 != s && s2.subsetOf(s)))
+    println(s"Solutions: $minSols, all: $solutions")
+
+    minSols
   }
 
   val ex1 = List(
     cont(A,B,0,5), reqs(B,C,20,25), reqs(B,D,20,25), cont(F,E,0,5), reqs(E,C,1,6), reqs(E,D,0,5)
   ).flatten
+  val ex2 = List(
+    cont(A,B,0,2), cont(B,C,0,2), reqs(C,D,0,2)
+  ).flatten
 
-  println("\nObserve example:")
-  assertDC(ex1)
+//  println("\nObserve example:")
+//  assertDC(ex1)
 
-  println("\nTwo non-obs:")
-  assertNotDC(makeNonObservable(ex1, List(B,E)))
+//  println("\nTwo non-obs:")
+//  assertNotDC(makeNonObservable(ex1, List(B,E)))
 
-  println("\nOne non-obs:")
-  assertDC(makeNonObservable(ex1, List(E)))
-  assertNotDC(makeNonObservable(ex1, List(B)))
+//  println("\nOne non-obs:")
+//  assertDC(makeNonObservable(ex1, List(E)))
+//  assertNotDC(makeNonObservable(ex1, List(B)))
+//
+//  getMinimalObservationSets(ex1)
 
+  getMinimalObservationSets(ex2)
 
   def cont(src:Node, dst: Node, minDur:Int, maxDur:Int) = {
-    List(Upper(dst, src, -maxDur, dst), Lower(src, dst, minDur, dst))
+    List(Upper(dst, src, -maxDur, dst, Set()), Lower(src, dst, minDur, dst, Set()))
   }
   def reqs(src:Node, dst:Node, min:Int, max:Int) = {
-    List(Req(src, dst, max), Req(dst, src, -min))
+    List(Req(src, dst, max, Set()), Req(dst, src, -min, Set()))
   }
 }
