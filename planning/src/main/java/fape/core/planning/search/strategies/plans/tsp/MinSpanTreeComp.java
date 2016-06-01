0d8cc950391e0f612c19267d871051270a071b18
Arthur Bit-Monnot <arthur.bit-monnot@laas.fr>
2016-06-01 19:25:46 +0200

    [planning] In minspan, use the number of timelines involved in threats instead of the number of threats.

--- planning/src/main/java/fape/core/planning/search/strategies/plans/tsp/MinSpanTreeComp.java
+++ planning/src/main/java/fape/core/planning/search/strategies/plans/tsp/MinSpanTreeComp.java
@@ -1,9 +1,13 @@
 package fape.core.planning.search.strategies.plans.tsp;
 
+import fape.core.planning.search.flaws.flaws.Threat;
 import fape.core.planning.search.strategies.plans.PartialPlanComparator;
 import fape.core.planning.states.State;
 
+import java.util.stream.Stream;
+
 public class MinSpanTreeComp extends PartialPlanComparator {
+    public static boolean USE_SUM = false;
     @Override
     public String shortName() {
         return "minspan";
@@ -25,13 +29,19 @@ public class MinSpanTreeComp extends PartialPlanComparator {
         return hc(st);
     }
 
+    private int threatsCost(State st) {
+        return (int) Stream.concat(
+                st.getAllThreats().stream().map(t -> ((Threat) t).db1),
+                st.getAllThreats().stream().map(t -> ((Threat) t).db2)).distinct().count();
+    }
+
     @Override
     public double hc(State st) {
-        return getExt(st).getCostToGo()+ st.tdb.getConsumers().size() + st.getAllThreats().size();
+        return getExt(st).getCostToGo()+ st.tdb.getConsumers().size() + threatsCost(st);
     }
 
     @Override
     public String reportOnState(State st) {
-        return shortName()+"f: "+(g(st)+h(st))+" g:"+g(st)+" h:"+h(st)+" hc: "+hc(st)+" = "+getExt(st).getCostToGo()+" + "+ st.tdb.getConsumers().size()+" + "+st.getAllThreats().size();
+        return shortName()+"f: "+(g(st)+h(st))+" g:"+g(st)+" h:"+h(st)+" hc: "+hc(st)+" = "+getExt(st).getCostToGo()+" + "+ st.tdb.getConsumers().size()+" + "+threatsCost(st);
     }
 }
