79315a5de2e4c318102f5eac8b7a9c5487cf386c
Arthur Bit-Monnot <arthur.bit-monnot@laas.fr>
2016-11-16 10:35:32 +0100

    [anml] Clean up unused method

--- anml-parser/src/main/scala/fr/laas/fape/anml/model/concrete/constraints.scala
+++ anml-parser/src/main/scala/fr/laas/fape/anml/model/concrete/constraints.scala
@@ -9,17 +9,14 @@ abstract class Constraint
 abstract class TemporalConstraint extends Constraint {
   def src : TPRef
   def dst : TPRef
-  def isParameterized : Boolean
 }
 
 case class MinDelayConstraint(src:TPRef, dst:TPRef, minDelay:IntExpression) extends TemporalConstraint {
   override def toString = s"$src + $minDelay <= $dst"
-  override def isParameterized = false
 }
 
 case class ContingentConstraint(src :TPRef, dst :TPRef, min :IntExpression, max :IntExpression) extends TemporalConstraint {
   override def toString = s"$src == [$min, $max] ==> $dst"
-  override def isParameterized = false
 }
 
 
