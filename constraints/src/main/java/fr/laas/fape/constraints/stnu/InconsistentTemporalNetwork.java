b02beb0c24e6e79ba8ec23155488008afa4fd931
Arthur Bit-Monnot <arthur.bit-monnot@laas.fr>
2016-11-20 16:01:39 +0100

    [constraint] Allow more detailed error messages to be thrown with InconsistentTemporalNetwork

--- constraints/src/main/java/fr/laas/fape/constraints/stnu/InconsistentTemporalNetwork.java
+++ constraints/src/main/java/fr/laas/fape/constraints/stnu/InconsistentTemporalNetwork.java
@@ -3,4 +3,9 @@ package fr.laas.fape.constraints.stnu;
 import fr.laas.fape.exceptions.InconsistencyException;
 
 public class InconsistentTemporalNetwork extends InconsistencyException {
+
+    public InconsistentTemporalNetwork() {}
+    public InconsistentTemporalNetwork(String msg) { super(msg); }
+    public InconsistentTemporalNetwork(Throwable e) { super(e); }
+    public InconsistentTemporalNetwork(String msg, Throwable e) {super(msg, e); }
 }
