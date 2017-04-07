b2c2038e3b9298863f1f5ef451754c087a50df12
Arthur Bit-Monnot <arthur.bit-monnot@laas.fr>
2017-04-07 13:05:47 +0200

    [constraints] Provide special assertion methods associated with runtime debug levels.

---
+++ constraints/src/main/scala/fr/laas/fape/constraints/meta/util/Assertion.scala
@@ -0,0 +1,65 @@
+package fr.laas.fape.constraints.meta.util
+
+import java.lang.management.ManagementFactory
+
+import scala.annotation.elidable
+import scala.annotation.elidable._
+
+/** Provides a flexible assertion support, related to a debug level.
+  *
+  * DEBUG_LEVEL == 1, should be suitable for a release version of the software, with inexpensive sanity checks.
+  * DEBUG_LEVEL 2 and 3, encompass more expensive sanity checks that should only be made in debug mode.
+  *
+  * Default debug level is 3 when java assertions are enabled (VM option "-ea") and 1 otherwise.
+  *
+  * All assert methods have lazy evaluation of the all parameters, so invocation should be almost for free when above
+  * the current DEBUG_LEVEL.
+  *
+  * Assertion can be remove at compile time with "-Xelide-below" parameter to scalac.
+  */
+object Assertion {
+
+  /** By default, debug level is set to 3 if java assertions are enabled (with VM option "-ea") and 1 otherwise */
+  var DEBUG_LEVEL =
+    if(ManagementFactory.getRuntimeMXBean.getInputArguments.contains("-ea"))
+      3
+    else
+      1
+
+  @elidable(ASSERTION) @inline
+  final def assert1(assertion: => Boolean, message: => Any) {
+    if (DEBUG_LEVEL >= 1 && !assertion)
+      throw new java.lang.AssertionError("assertion failed: "+ message)
+  }
+
+  @elidable(FINE) @inline
+  final def assert2(assertion: => Boolean, message: => Any) {
+    if (DEBUG_LEVEL >= 2 && !assertion)
+      throw new java.lang.AssertionError("assertion failed: "+ message)
+  }
+
+  @elidable(FINEST) @inline
+  final def assert3(assertion: => Boolean, message: => Any) {
+    if (DEBUG_LEVEL >= 3 && !assertion)
+      throw new java.lang.AssertionError("assertion failed: "+ message)
+  }
+
+  @elidable(ASSERTION) @inline
+  final def assert1(assertion: => Boolean) {
+    if (DEBUG_LEVEL >= 1 && !assertion)
+      throw new java.lang.AssertionError("assertion failed")
+  }
+
+  @elidable(FINE) @inline
+  final def assert2(assertion: => Boolean) {
+    if (DEBUG_LEVEL >= 2 && !assertion)
+      throw new java.lang.AssertionError("assertion failed")
+  }
+
+  @elidable(FINEST) @inline
+  final def assert3(assertion: => Boolean) {
+    if (DEBUG_LEVEL >= 3 && !assertion)
+      throw new java.lang.AssertionError("assertion failed")
+  }
+
+}
---
+++ constraints/src/test/scala/fr/laas/fape/constraints/meta/AssertionLevelTest.scala
@@ -0,0 +1,26 @@
+package fr.laas.fape.constraints.meta
+
+import fr.laas.fape.constraints.meta.util.Assertion._
+import org.scalatest.FunSuite
+
+class AssertionLevelTest extends FunSuite {
+
+  test("Print assertion levels") {
+    print("Enabled assertion levels: ")
+    assert1({print(" 1"); true}, "")
+    assert2({print(" 2"); true}, "")
+    assert3({print(" 3"); true}, "")
+    println()
+  }
+
+  test("Assertion lazy evaluation") {
+
+    var assertionLevel = 0
+    assert1({assertionLevel = 1; true}, "")
+    assert2({assertionLevel = 2; true}, "")
+    assert3({assertionLevel = 3; true}, "")
+
+    assert(assertionLevel == DEBUG_LEVEL)
+  }
+
+}
