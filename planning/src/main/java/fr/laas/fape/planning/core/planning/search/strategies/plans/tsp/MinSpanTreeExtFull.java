d3ecb59b8303bd87b1b40e980f78d83fe53bf76a
Arthur Bit-Monnot <arthur.bit-monnot@laas.fr>
2018-12-04 18:03:20 +0100

    Avoid most allocations in min-span heuristic.

--- planning/src/main/java/fr/laas/fape/planning/core/planning/search/strategies/plans/tsp/MinSpanTreeExtFull.java
+++ planning/src/main/java/fr/laas/fape/planning/core/planning/search/strategies/plans/tsp/MinSpanTreeExtFull.java
@@ -17,15 +17,29 @@ import fr.laas.fape.planning.exceptions.NoSolutionException;
 import fr.laas.fape.planning.util.Pair;
 import fr.laas.fape.structures.DijkstraQueue;
 import fr.laas.fape.structures.IRSet;
+import fr.laas.fape.structures.IntRep;
 import lombok.RequiredArgsConstructor;
 import lombok.Value;
 import fr.laas.fape.anml.model.ParameterizedStateVariable;
 
 import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @RequiredArgsConstructor
 public class MinSpanTreeExtFull implements StateExtension {
+
+    private static class Level {
+        Map<Integer,IRSet<Fluent>> map = new HashMap<>();
+
+        boolean containsKey(Fluent f) { return map.containsKey(f.getID()); }
+        boolean containsKey(int id) { return map.containsKey(id); }
+        IRSet<Fluent> get(Fluent f) { return map.get(f.getID()); }
+        IRSet<Fluent> get(int id) { return  map.get(id); }
+        void put(Fluent f, IRSet<Fluent> fs) { map.put(f.getID(), fs); }
+        void computeIfAbsent(Fluent f, Function<Integer, IRSet<Fluent>> prod) { map.computeIfAbsent(f.getID(), prod); }
+    }
 
     private final boolean USE_SUM = GlobalOptions.getBooleanOption("heur-additive-pending-cost");
     private int dbgLvl = 0;
@@ -37,15 +51,25 @@ public class MinSpanTreeExtFull implements StateExtension {
     private final HashMap<LogStatement, HashSet<GAction.GLogStatement>> groundStatements = new HashMap<>();
 
     public final Map<Timeline,List<Integer>> allCosts;
+    private IntRep<Fluent> fluentRep;
+
+    private IRSet<Fluent> workingCopy1;
+    private IRSet<Fluent> workingCopy2;
 
     public MinSpanTreeExtFull(PartialPlan st) {
         this.st = st;
         allCosts = new HashMap<>();
+        fluentRep = st.pl.preprocessor.store.getIntRep(Fluent.class);
+        workingCopy1 = new IRSet<Fluent>(fluentRep);
+        workingCopy2 = new IRSet<Fluent>(fluentRep);
     }
 
     public MinSpanTreeExtFull(PartialPlan st, MinSpanTreeExtFull toCopy) {
         this.st = st;
         this.allCosts = new HashMap<>(toCopy.allCosts);
+        fluentRep = st.pl.preprocessor.store.getIntRep(Fluent.class);
+        workingCopy1 = new IRSet<Fluent>(fluentRep);
+        workingCopy2 = new IRSet<Fluent>(fluentRep);
     }
 
     @Override
@@ -89,13 +113,13 @@ public class MinSpanTreeExtFull implements StateExtension {
 
     private class TimelineDTG {
         Timeline tl;
-        List<Map<Fluent,Set<Fluent>>> precedingFluents = new ArrayList<>();
+        List<Level> precedingFluents = new ArrayList<>();
 
         TimelineDTG(Timeline tl) {
             this.tl = tl;
             assert !tl.hasSinglePersistence();
             for(int i=0; i< tl.numChanges() ; i++) {
-                precedingFluents.add(new HashMap<>());
+                precedingFluents.add(new Level());
                 LogStatement e = tl.getEvent(i);
                 for(GAction.GLogStatement ge : st.getGroundStatements(e)) {
                     if (!e.needsSupport()) {
@@ -108,23 +132,34 @@ public class MinSpanTreeExtFull implements StateExtension {
                     } else {
                         if(i > 0 && !precedingFluents.get(i-1).containsKey(ge.getStartFluent()))
                             continue; // there is no path to this fluent
-                        this.precedingFluents.get(i).computeIfAbsent(ge.getEndFluent(), x -> new HashSet<>());
+                        this.precedingFluents.get(i).computeIfAbsent(ge.getEndFluent(), x -> new IRSet<Fluent>(fluentRep));
                         this.precedingFluents.get(i).get(ge.getEndFluent()).add(ge.getStartFluent());
                     }
                 }
             }
         }
 
-        Collection<Fluent> leftMostsFrom(int level, Collection<Fluent> sources) {
+        IRSet<Fluent> leftMostsFrom(int level, IRSet<Fluent> sources) {
+            workingCopy1.clear();
+            workingCopy2.clear();
+            workingCopy1.addAll(sources);
+            return leftMostsFromImpl(level, workingCopy1);
+        }
+        private IRSet<Fluent> leftMostsFromImpl(int level, IRSet<Fluent> sources) {
+            assert sources == workingCopy1 || sources == workingCopy2;
+            IRSet<Fluent> output = sources == workingCopy1 ? workingCopy2 : workingCopy1;
             assert tl.isConsumer();
-            Set<Fluent> previousFluents = sources.stream()
-                    .filter(f-> precedingFluents.get(level).containsKey(f))
-                    .flatMap(f -> precedingFluents.get(level).get(f).stream())
-                    .collect(Collectors.toSet());
+//            Fluent[] ss = sources.toArray(new Fluent[0]);
+            Level pre = precedingFluents.get(level);
+            output.clear();
+            sources.stream().forEach(i -> {
+                if(pre.containsKey(i))
+                    output.addAll(pre.get(i));
+            });
             if(level == 0) {
-                return previousFluents;
+                return output;
             } else {
-                return leftMostsFrom(level-1, previousFluents);
+                return leftMostsFromImpl(level-1, output);
             }
         }
 
@@ -132,6 +167,7 @@ public class MinSpanTreeExtFull implements StateExtension {
             return precedingFluents.get(precedingFluents.size()-1).containsKey(f);
         }
     }
+
 
     private void processTimelines() {
         for(Timeline tl : st.getTimelines()) {
@@ -162,7 +198,7 @@ public class MinSpanTreeExtFull implements StateExtension {
                     if (e.getChangeNumber() < sup.numChanges() - 1) {
                         if (sup.isConsumer()) {
                             // traverse this timeline and add any fluent that can be used to support 'sup' to the queue
-                            Collection<Fluent> supStartFluents = timelineDTGs.get(sup).leftMostsFrom(e.getChangeNumber(), startFluents(og.getFirst().getFirst()));
+                            IRSet<Fluent> supStartFluents = timelineDTGs.get(sup).leftMostsFrom(e.getChangeNumber(), startFluents(og.getFirst().getFirst()));
                             for (Fluent f : supStartFluents) {
                                 startNodes.add(new Pair<>(new DijNode(f, sup), 1));
                             }
@@ -245,7 +281,7 @@ public class MinSpanTreeExtFull implements StateExtension {
                 Timeline sup = st.getTimeline(e.supporterID);
                 if(e.getChangeNumber() == sup.numChanges()-1 && timelineDTGs.get(sup).canSupportAtEnd(cur.f)) {
                     if(sup.isConsumer()) {
-                        for(Fluent left : timelineDTGs.get(sup).leftMostsFrom(e.getChangeNumber(), Collections.singleton(cur.f))) {
+                        for(Fluent left : timelineDTGs.get(sup).leftMostsFrom(e.getChangeNumber(), IRSet.ofSingleton(fluentRep, cur.f))) {
                             q.putIfBetter(new DijNode(left,  sup), curCost+1, 1, cur);
                         }
                     } else {
@@ -297,8 +333,8 @@ public class MinSpanTreeExtFull implements StateExtension {
     private Fluent startFluent(GAction.GLogStatement s) {
         return st.pl.preprocessor.getFluent(s.sv, s.startValue());
     }
-    private Set<Fluent> startFluents(LogStatement s) {
-        IRSet<Fluent> fs =new IRSet<>(st.pl.preprocessor.store.getIntRep(Fluent.class));
+    private IRSet<Fluent> startFluents(LogStatement s) {
+        IRSet<Fluent> fs = new IRSet<>(st.pl.preprocessor.store.getIntRep(Fluent.class));
         getGrounded(s).stream().forEach(gs -> fs.add(startFluent(gs)));
         return fs;
     }
